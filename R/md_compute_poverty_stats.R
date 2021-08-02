1c981f6fbe00923babe7d98ef9bb55a7441cf129
R.Andrés Castañeda <acastanedaa@worldbank.org>
2021-08-02 11:27:11 -0400

    remove = sign from expression

--- R/md_compute_poverty_stats.R
+++ R/md_compute_poverty_stats.R
@@ -24,7 +24,7 @@ md_compute_poverty_stats <- function(welfare, weight, povline_lcu) {
   welfare <- welfare[o]
   weight  <- weight[o]
 
-  pov_status        <- (welfare <= povline_lcu)
+  pov_status        <- (welfare < povline_lcu)
   relative_distance <- (1 - (welfare[pov_status] / povline_lcu))
   non_pov           <- rep(0, collapse::fsum(!pov_status))
 
