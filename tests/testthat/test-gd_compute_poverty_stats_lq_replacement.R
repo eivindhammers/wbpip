f954e7ac67e065ed9e1e789878876a38f17e5cde
Diana Garcia <dana89co@hotmail.com>
2024-01-18 11:53:23 +0100

    Test for headcount negative

--- tests/testthat/test-gd_compute_poverty_stats_lq_replacement.R
+++ tests/testthat/test-gd_compute_poverty_stats_lq_replacement.R
@@ -204,3 +204,72 @@ test_that("gd_compute_poverty_stats_lq_replacement works as gd_compute_poverty_s
   expect_equal(round(out$dl, 7), round(benchmark$dl, 7))
   expect_equal(round(out$ddl, 6), round(benchmark$ddl, 6))
 })
+
+test_that("gd_compute_poverty_stats_lq_replacement works with negative headcount", {
+  mean <- 51.5660557757944
+  povline <- -57.791666666666664 # Negative to test negative headcount
+  A <- 0.795981535745657
+  B <- -1.4445933880119242
+  C <- 0.14728191995919815
+  e <- -0.498670067692931
+  m <- -1.0970760862948583
+  n <- 0.851623285340541
+  r <- 1.3477796260474386
+  s1 <- -0.22612667749534146
+  s2 <- 1.002393060455814
+
+  # Test values above make headcount negative
+  # gd_compute_headcount_lq(
+  #   mean    = mean,
+  #   povline = povline,
+  #   B       = B,
+  #   m       = m,
+  #   n       = n,
+  #   r       = r
+  # )
+
+  benchmark <- gd_compute_poverty_stats_lq(
+    mean = mean,
+    povline = povline,
+    A = A,
+    B = B,
+    C = C,
+    e = e,
+    m = m,
+    n = n,
+    r = r,
+    s1 = s1,
+    s2 = s2)
+
+  out <- gd_compute_poverty_stats_lq_replacement(
+    mean = mean,
+    povline = povline,
+    A = A,
+    B = B,
+    C = C,
+    e = e,
+    m = m,
+    n = n,
+    r = r,
+    s1 = s1,
+    s2 = s2
+  )
+
+  expect_equal(length(out), length(benchmark))
+  expect_equal(names(out), c(
+    "headcount", "pg", "p2", "eh", "epg",
+    "ep", "gh", "gpg", "gp", "watts", "dl", "ddl"
+  ))
+  expect_equal(round(out$headcount, 7), round(benchmark$headcount, 7))
+  expect_equal(out$pg, benchmark$pg)
+  expect_equal(out$p2, benchmark$p2)
+  expect_equal(round(out$eh, 6), round(benchmark$eh, 6)) # Due to headcount difference
+  expect_equal(round(out$epg, 7), round(benchmark$epg, 7)) # Due to headcount difference
+  expect_equal(out$ep, benchmark$ep)
+  expect_equal(out$gh, benchmark$gh, tolerance = 1.1e-07)
+  expect_equal(out$gpg, benchmark$gpg)
+  expect_equal(out$gp, benchmark$gp)
+  expect_equal(out$watts, benchmark$watts)
+  expect_equal(round(out$dl, 7), round(benchmark$dl, 7))
+  expect_equal(round(out$ddl, 6), round(benchmark$ddl, 6))
+})
